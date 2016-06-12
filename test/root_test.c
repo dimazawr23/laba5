b8e736896375bb045923408848c31dfaa8ddc6fb
dimazawr23 <dimazawr24@gmail.com>
2016-06-13 03:37:55 +0600

    2 more tests

--- test/root_test.c
+++ test/root_test.c
@@ -21,3 +21,44 @@ CTEST(quadratic_equation_suite, discriminant_test) {
     ASSERT_DBL_NEAR(expected_answer.discriminant, answer.discriminant);
     ASSERT_DBL_NEAR(expected_status, status);
 }
+CTEST(quadratic_equation_suite, two_roots_test) {
+    // Given
+    const float a = 5.4;
+    const float b = 32;
+    const float c = 1;
+
+    // When
+
+    Roots answer;
+    int status;
+    answer = solve(a, b, c, &status);
+
+    // Then
+    const int expected_status = TWO_ROOTS;
+    const Roots expected_answer = {-0.031416556251127374, -5.894509369674798, 1002.4};
+
+    ASSERT_DBL_NEAR(expected_answer.discriminant, answer.discriminant);
+    ASSERT_DBL_NEAR(expected_status, status);
+    ASSERT_DBL_NEAR(expected_answer.x1, answer.x1);
+    ASSERT_DBL_NEAR(expected_answer.x2, answer.x2);
+}
+
+CTEST(quadratic_equation_suite, negative_discriminant_test) {
+    // Given
+    const float a = 4;
+    const float b = 1;
+    const float c = 4;
+
+    // When
+    Roots answer;
+    int status;
+    answer = solve(a, b, c, &status);
+
+
+    // Then
+    const int expected_status = NO_ROOTS;
+    const Roots expected_answer = {.discriminant = -63};
+
+    ASSERT_DBL_NEAR(expected_answer.discriminant, answer.discriminant);
+    ASSERT_DBL_NEAR(expected_status, status);    
+}
