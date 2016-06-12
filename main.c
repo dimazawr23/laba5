52139debc65b6c1fe558170514e36c25f60ced95
dimazawr23 <dimazawr24@gmail.com>
2016-06-13 01:33:32 +0600

    Add discriminant conditions

--- main.c
+++ main.c
@@ -12,5 +12,12 @@ int main()
 	printf("Input your A, B, C for equation\n");
 	scanf("%d %d %d", &a, &b, &c);
 	d = b * b - 4 * a * c;
+	if (d >= 0) {
+		x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+	}
+	else
+		printf("Discriminant is negative\n");
+
 	return 0;
 }
