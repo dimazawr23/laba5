1b4dc4ca47b0f1c92cce6bced06fcd6938af0d98
dimazawr23 <dimazawr24@gmail.com>
2016-06-13 03:37:01 +0600

    added first test

--- test/root_test.c
+++ test/root_test.c
@@ -1,2 +1,23 @@
 #include <ctest.h>
 #include <root.h>
+#define UNUSED(x) (void)(x)
+
+CTEST(quadratic_equation_suite, discriminant_test) {
+    // Given
+    const float a = 1;
+    const float b = 2;
+    const float c = 1;
+    
+    // When
+    Roots answer;
+    int status;
+    answer = solve(a, b, c, &status);
+
+    // Then
+    const Roots expected_answer = {-1, -1, 0};
+    const int expected_status = ONE_ROOT;
+
+    ASSERT_DBL_NEAR(expected_answer.x1, answer.x1);
+    ASSERT_DBL_NEAR(expected_answer.discriminant, answer.discriminant);
+    ASSERT_DBL_NEAR(expected_status, status);
+}
